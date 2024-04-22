0be21547aacd05ec054e6adeb34c5f578962fcbd
James Rowe <jnrowe@gmail.com>
2024-04-22 19:22:07 +0100

    feat: Add simplistic todo.txt parser
    This is *far* too lax for real world use, but it is complete enough to
    start play around with the data to see how the file format will hold up.

---
+++ todo_txt.py
@@ -0,0 +1,72 @@
+import datetime
+import enum
+import re
+
+from attrs import define
+
+
+Priority = enum.IntEnum(
+    "Priority", "Z Y X W V U T S R Q P O N M L K J I H G F E D C B A"
+)
+
+
+@define
+class Context:
+    name: str
+
+
+@define
+class Project:
+    name: str
+
+
+@define
+class Entry:
+    text: str
+    complete: bool = False
+    completion_date: datetime.date | None = None
+    creation_date: datetime.date | None = None
+    priority: Priority | None = None
+    contexts: list[Context] = []
+    projects: list[Project] = []
+    attrs: dict[str, str | datetime.date] = {}
+
+
+def parse_data(text: str) -> Entry:
+    d_str = r"\d{4}-\d{2}-\d{2}"
+    if m := re.match(rf"x (?:({d_str})(?: {d_str})?)?", text):
+        complete = True
+        if m.lastindex:
+            completion_date = datetime.date.fromisoformat(m.group(1))
+        else:
+            completion_date = None
+    else:
+        complete = False
+        completion_date = None
+    if m := re.match(r"(?:x )?\(([A-Z])\) ", text):
+        prio = Priority[m.group(1)]
+    else:
+        prio = None
+    if m := re.match(rf"(?:x {d_str} |\([A-Z]\) )?({d_str}) ", text):
+        creation_date = datetime.date.fromisoformat(m.group(1))
+    else:
+        creation_date = None
+    cs = []
+    ps = []
+    for t, v in re.findall(r"(?<=\s)([@\+])(\S+)", text):
+        if t == "@":
+            cs.append(Context(v))
+        else:
+            ps.append(Project(v))
+    kws: dict[str, str | datetime.date] = {}
+    for k, v in re.findall(r"([^\s:]+):([^\s:]+)", text):
+        if k == "pri":
+            prio = Priority[v]
+        else:
+            try:
+                kws[k] = datetime.date.fromisoformat(v)
+            except ValueError:
+                kws[k] = v
+    return Entry(
+        text, complete, completion_date, creation_date, prio, cs, ps, kws
+    )
