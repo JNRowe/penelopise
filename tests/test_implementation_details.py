52fd8164823dced04480b3f2b95da74de75a6975
James Rowe <jnrowe@gmail.com>
2025-08-18 05:42:58 +0100

    test: Check priority as metadata is not in attrs

--- tests/test_implementation_details.py
+++ tests/test_implementation_details.py
@@ -8,3 +8,10 @@ def test_set_cached_property():
     entry = penelopise.Entry("some task")
     with pytest.raises(AttributeError):
         entry.complete = True
+
+
+def test_priority_with_pri_tag():
+    """Test priority as metadata is not duplicated."""
+    entry = penelopise.Entry("some task pri:A")
+    assert entry.priority == penelopise.Priority.A
+    assert entry.attrs == {}
