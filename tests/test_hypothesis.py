a9e8fba480ba4ff7b9a1e3b86af1333770d3dd38
James Rowe <jnrowe@gmail.com>
2025-08-21 20:13:24 +0100

    refactor(test): Further increase input strategy robustness

--- tests/test_hypothesis.py
+++ tests/test_hypothesis.py
@@ -1,35 +1,54 @@
 import datetime
 import string
+import typing
 
+import attrs
 from hypothesis import given
 from hypothesis import strategies as st
 
 import penelopise
 
 
+@attrs.define
+class TodoData:
+    contexts: list[str]
+    projects: list[str]
+    attrs: dict[str, str | datetime.date]
+    priority: str | None
+    complete: bool
+    completion_date: datetime.date | None
+    creation_date: datetime.date | None
+
+
 # According to the spec, a project or context contains any non-whitespace
 # character.  This is a bit broad for a generation strategy, so we'll limit it
 # to something more reasonable.
-safe_text = st.text(
+safe_tags = st.text(
     alphabet=st.characters(
         categories=["L", "N"],
-        exclude_characters="+@:",
     ),
     min_size=1,
 )
 
+# Filler text is largely unrestricted; but not the shape of a context, project,
+# or attribute pair
+safe_text = st.text(
+    alphabet=st.characters(
+        exclude_characters=":",
+    ).filter(lambda s: not s.startswith(("+", "@"))),
+)
+
 
 @st.composite
-def todo_entries(draw):
+def todo_data(draw) -> TodoData:
     is_complete = draw(st.booleans())
     has_priority = draw(st.booleans())
     has_creation_date = draw(st.booleans())
 
-    completion_marker = "x " if is_complete else ""
     priority = (
         draw(st.sampled_from(string.ascii_uppercase))
         if has_priority and not is_complete
-        else ""
+        else None
     )
     creation_date = draw(
         st.dates(
@@ -44,73 +63,90 @@ def todo_entries(draw):
         )
     )
 
+    projects = draw(st.sets(safe_tags))
+    contexts = draw(st.sets(safe_tags))
+    attrs = draw(st.dictionaries(safe_tags, safe_tags))
+    if "pri" in attrs:
+        if priority:
+            del attrs["pri"]
+        else:
+            # We'll overwrite this one keyword to match expected values
+            attrs["pri"] = draw(st.sampled_from(string.ascii_uppercase))
+
+    if is_complete:
+        priority = None
+        if not has_creation_date:
+            creation_date = None
+    else:
+        if not has_priority:
+            priority = None
+        completion_date = None
+        if has_creation_date:
+            creation_date = None
+
+    return TodoData(
+        contexts=contexts,
+        projects=projects,
+        attrs=attrs,
+        priority=priority,
+        complete=is_complete,
+        completion_date=completion_date,
+        creation_date=creation_date,
+    )
+
+
+@st.composite
+def todo_testable(draw) -> tuple[str, TodoData]:
+    data = draw(todo_data())
+
+    completion_marker = "x " if data.complete else ""
     words = draw(st.lists(safe_text))
-    projects = draw(st.sets(safe_text))
-    contexts = draw(st.sets(safe_text))
-    kv_pairs = draw(st.dictionaries(safe_text, safe_text))
 
     description_parts = (
         words
-        + [f"+{s}" for s in projects]
-        + [f"@{s}" for s in contexts]
-        + [f"{k}:{v}" for k, v in kv_pairs.items()]
+        + [f"+{s}" for s in data.projects]
+        + [f"@{s}" for s in data.contexts]
+        + [f"{k}:{v}" for k, v in data.attrs.items()]
     )
     draw(st.randoms()).shuffle(description_parts)
     description = " ".join(description_parts)
 
-    # Construct the final entry
     entry = ""
-    known_valid = {
-        "contexts": contexts,
-        "projects": projects,
-        "attrs": kv_pairs,
-    }
-    if is_complete:
-        known_valid["priority"] = None
-        known_valid["complete"] = True
+    if data.complete:
         entry += completion_marker
-        known_valid["completion_date"] = completion_date
-        entry += completion_date.strftime("%F ")
-        if has_creation_date:
-            known_valid["creation_date"] = creation_date
-            entry += creation_date.strftime("%F ")
-        else:
-            known_valid["creation_date"] = None
+        entry += data.completion_date.strftime("%F ")
+        if data.creation_date:
+            entry += data.creation_date.strftime("%F ")
     else:
-        if has_priority:
-            known_valid["priority"] = priority
-            entry += f"({priority}) "
-        else:
-            known_valid["priority"] = None
-        known_valid["complete"] = False
-        known_valid["completion_date"] = None
-        if has_creation_date:
-            known_valid["creation_date"] = creation_date
-            entry += creation_date.strftime("%F ")
-        else:
-            known_valid["creation_date"] = None
+        if data.priority:
+            entry += f"({data.priority}) "
+        if data.creation_date:
+            entry += data.creation_date.strftime("%F ")
 
     entry += description
 
-    return entry, known_valid
+    return entry, data
 
 
-@given(todo_entries())
-def test_fuzz_parse(data):
-    text, known_valid = data
-    parsed = penelopise.Entry(text)
-    assert parsed.complete == known_valid["complete"]
-    if known_valid["priority"]:
-        assert parsed.priority.name == known_valid["priority"]
+@given(todo_testable())
+def test_fuzz_parse(testable):
+    entry, known = testable
+    parsed = penelopise.Entry(entry)
+    assert parsed.complete == known.complete
+    if known.priority:
+        assert (
+            typing.cast(penelopise.Priority, parsed.priority).name
+            == known.priority
+        )
+    elif "pri" in known.attrs:
+        pri = known.attrs.pop("pri")
+        assert typing.cast(penelopise.Priority, parsed.priority).name == pri
     else:
         assert parsed.priority is None
     # Order is unimportant for contexts and projects
-    assert set(parsed.contexts) == known_valid["contexts"]
-    assert set(parsed.projects) == known_valid["projects"]
-    if "pri" in known_valid["attrs"]:
-        pri = known_valid["attrs"].pop("pri")
-        assert parsed.priority.name == pri
-    assert parsed.attrs == known_valid["attrs"]
-    assert parsed.complete is known_valid["complete"]
-    assert parsed.completion_date == known_valid["completion_date"]
-    assert parsed.creation_date == known_valid["creation_date"]
+    assert set(parsed.contexts) == known.contexts
+    assert set(parsed.projects) == known.projects
+    assert parsed.attrs == known.attrs
+    assert parsed.complete is known.complete
+    assert parsed.completion_date == known.completion_date
+    assert parsed.creation_date == known.creation_date
